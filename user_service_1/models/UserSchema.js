00bfd101caa961ee5674f811e0ce736d996eca22
udarasan <udarassanjeewa@gmail.com>
2021-06-28 21:49:53 +0530

    user controller

--- user_service_1/models/UserSchema.js
+++ user_service_1/models/UserSchema.js
@@ -5,3 +5,38 @@
 * dotenv
 *
 * * */
+
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    fName:{
+        type: String,
+        required: true
+    },
+    lName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    avatar:{
+        type:String
+    },
+    userState:{
+        type:Boolean
+    },
+    regDate:{
+        type:Date,
+        required:true
+    },
+    regTime:{
+        type:String,
+        required:true
+    }
+});
+module.exports = mongoose.model('User', UserSchema);
